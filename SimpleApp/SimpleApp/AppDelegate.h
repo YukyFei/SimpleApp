65c42727dfcd4fea4b46198fa3020980002464ce
fei yb <331035175@qq.com>
2019-09-21 21:36:20 +0800

    工程初始化+pod

---
+++ SimpleApp/SimpleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleApp
+//
+//  Created by huaxuezoo on 2019/9/21.
+//  Copyright © 2019 huaxuezoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
